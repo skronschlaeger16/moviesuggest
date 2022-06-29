5f5ea9df2777f28385f4e09908e84993c163e3f8
byFloriiian <florian.eggetsbergerfefe@gmail.com>
2022-06-29 23:34:28 +0200

    added basic authentication

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { LoginPage } from '../auth/login/login.page';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private afAuth: AngularFireAuth) {}
 
-  constructor() { }
+  loginWithEmailAndPassword(email, password) {
+    this.afAuth
+      .signInWithEmailAndPassword(email, password)
+      .then((value) => {
+        console.log('Logged in Successfully');
+      })
+      .catch((err) => {
+        console.log('Something went wrong: ', err.message);
+      });
+  }
+
+  registerWithEmailAndPassword(username, email, password) {
+    this.afAuth
+      .createUserWithEmailAndPassword(email, password)
+      .then((value) => {
+        console.log('Created User: ', value);
+      })
+      .catch((err) => {
+        console.log('Something went wrong: ', err);
+      });
+  }
+
+  logout(){
+    this.afAuth.signOut()
+    .then(() => {
+      console.log('Logged out Successfully!');
+    })
+    .catch((err) => {
+      console.log('Something went wrong: ', err);
+    });
+  }
 }
